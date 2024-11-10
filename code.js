f7442af5aee673e197ada43bba14d6621c462797
Valentino Menndoza <joshuavale28@gmail.com>
2024-11-10 17:20:05 -0500

    Se agrego hoja de estilos

--- code.js
+++ code.js
@@ -1,12 +1,12 @@
 //Numero aleatorio
-let numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+// let numeroAleatorio = Math.floor(Math.random() * 10) + 1;
 
 //Numero del usuario
-let numeroUsuario = prompt('Ingrese un numero del 1 al 10!');
+// let numeroUsuario = prompt('Ingrese un numero del 1 al 10!');
 //Convertimos el numero del usuario en entero
-let numeroUsuarioEntero = parseInt(numeroUsuario);
+//let numeroUsuarioEntero = parseInt(numeroUsuario);
 
 //Creamos la condición para validar el numero
-numeroAleatorio == numeroUsuario ? alert('Numero acertado') : alert('Numero incorrecto');
+//numeroAleatorio == numeroUsuario ? alert('Numero acertado') : alert('Numero incorrecto');
 
-console.log(numeroAleatorio);
+//console.log(numeroAleatorio);
