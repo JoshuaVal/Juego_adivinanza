462f04059aa8fcd7c855b33cea9d142fea5b6332
Valentino Menndoza <joshuavale28@gmail.com>
2024-11-13 17:54:03 -0500

    terminado

--- code.js
+++ code.js
@@ -1,8 +1,5 @@
-// Obtener el botón por su id
+// Obtener el botón 
 const boton = document.getElementById('botonJugar');
-
-// Obtener el párrafo donde mostraremos los resultados
-const resultado = document.getElementById('resultado');
 
 // Asociar la función al evento click del botón
 boton.addEventListener('click', iniciarJuego);
@@ -15,7 +12,7 @@ let numeroAleatorio = Math.floor(Math.random() * 10) + 1;
 let intentosMaximos = 3;
 let intentosRestantes = intentosMaximos;
 
-// Bucle para permitir múltiples intentos
+// Bucle while
 while (intentosRestantes > 0) {
     // Pedir al usuario que ingrese un número
     let numeroUsuario = prompt('Ingrese un número del 1 al 10:');
@@ -24,7 +21,7 @@ while (intentosRestantes > 0) {
     // Comprobar si el número es correcto
     if (numeroUsuarioEntero === numeroAleatorio) {
         alert('¡Felicidades! Adivinaste el número.');
-        break; // Salir del bucle si adivinó
+        break;
     } else {
         intentosRestantes--;
         alert(`¡Incorrecto! Te quedan: ${intentosRestantes}, intentos`);
@@ -33,6 +30,6 @@ while (intentosRestantes > 0) {
 
 // Si se agotaron los intentos, mostrar el número correcto
 if (intentosRestantes === 0) {
-    alert(`¡Se acabaron los intentos! El número era: ${numeroAleatorio}`);
+    alert(`¡Perdiste! El número era: ${numeroAleatorio}`);
 }
 }
